1a364646b28146001c85c3b77c878a024fa34fdf
loolys <pierre.liljekvist@outlook.com>
2016-12-02 14:28:09 +0100

    Added a working countdown timer that starts with a button press

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-var myInterval = window.setInterval(myCallBack, 1000);
-var myTime = 60;
+
+var myTime = 60*25;
 function myCallBack() {
-    if (myTime >= 0) {
+    if (myTime > 0) {
         myTime -= 1;
+        var minutes = Math.floor(myTime / 60).toString();
+        var seconds = (myTime - minutes * 60).toString();
         console.log(myTime);
+        $("#time").text(minutes + ":" + seconds);
     }
 }
+
+$(document).ready(function () {
+    $("#start").click(function(){
+        var myInterval = window.setInterval(myCallBack, 1000);
+    });
+});
