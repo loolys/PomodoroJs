05195888ab247bbed2a6aacf757c77e702dd84c6
loolys <pierre.liljekvist@outlook.com>
2016-12-05 14:29:52 +0100

    Made updates to the GUI to more reflect the final product

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
-
+var myInterval;
 var myTime = 60*25;
+function pPTime(string, pad, length){
+    return (new Array(length+1).join(pad)+string).slice(-length);
+}
+
 function myCallBack() {
     if (myTime > 0) {
         myTime -= 1;
         var minutes = Math.floor(myTime / 60).toString();
         var seconds = (myTime - minutes * 60).toString();
+        var formattedTime = pPTime(minutes, "0",2) +
+            ":" + pPTime(seconds,"0",2);
         console.log(myTime);
-        $("#time").text(minutes + ":" + seconds);
+        $("#time").text(formattedTime);
     }
 }
 
 $(document).ready(function () {
     $("#start").click(function(){
-        var myInterval = window.setInterval(myCallBack, 1000);
+        myInterval = window.setInterval(myCallBack, 1000);
     });
+    $("#stop").click(function (){
+        clearInterval(myInterval);
+    })
 });
